b4097f0f529001053eefcf486054da84f67fe4e4
Louis <louisfon@orange.fr>
2022-09-22 01:23:01 +0200

    create component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup>
+<script>
+  import LoginVue from './components/Login.vue'; 
+  
+import Navbar from './components/Navbar.vue'; 
+  export default {
+    name: "App",
+    components: {
+    LoginVue,
+    Navbar
+}
+  }
 </script>
 
 <template>
-  <h1>START</h1>
+  <Navbar/>
+  <router-view></router-view>
 </template>
 
-<style scoped>
+  
+<style lang="scss">
+  $primary-color: #FD2D01;
+  $secondary-color: #FFD7D7;
+  $tertiary-color: #4E5166;
+  
 </style>
